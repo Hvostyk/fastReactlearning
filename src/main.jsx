b6d7259db58b92e442c0ac2149a3ab3aae993268
Hvostyk <natlitvinandmat@gmail.com>
2025-01-30 21:40:22 +0300

    deleted usless dir

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import '../../frontsvo/styles/reset.css';
+import './styles/reset.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
